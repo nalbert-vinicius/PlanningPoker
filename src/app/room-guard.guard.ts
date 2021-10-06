d42fc218ef5e994a401037eff092ee21bde4f7cf
Nalbert, Vinicius <rullaxfox@gmail.com>
2021-10-06 19:43:05 -0300

    guard

--- src/app/room-guard.guard.ts
+++ src/app/room-guard.guard.ts
@@ -6,10 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class RoomGuardGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
+
+  
   
 }
